899e748d786544abf97260b0df915c2871be6e7a
guilhhotina <lottiecake@proton.me>
2026-05-26 18:58:44 -0300

    feat: add torsion motor

---
+++ src/main/java/com/chaosteam/chaosnkinetics/content/kinetics/torsion_motor/TorsionMotorGenerator.java
@@ -0,0 +1,34 @@
+package com.chaosteam.chaosnkinetics.content.kinetics.torsion_motor;
+
+import com.simibubi.create.foundation.data.SpecialBlockStateGen;
+import com.tterrag.registrate.providers.DataGenContext;
+import com.tterrag.registrate.providers.RegistrateBlockstateProvider;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+
+public class TorsionMotorGenerator extends SpecialBlockStateGen {
+    @Override
+    protected Property<?>[] getIgnoredProperties() {
+        return new Property<?>[] { TorsionMotorBlock.MODE };
+    }
+
+    @Override
+    protected int getXRotation(BlockState state) {
+        Direction facing = state.getValue(TorsionMotorBlock.FACING);
+        return facing == Direction.DOWN ? 180 : 0;
+    }
+
+    @Override
+    protected int getYRotation(BlockState state) {
+        Direction facing = state.getValue(TorsionMotorBlock.FACING);
+        return facing.getAxis().isVertical() ? 0 : horizontalAngle(facing);
+    }
+
+    @Override
+    public <T extends Block> ModelFile getModel(DataGenContext<Block, T> ctx, RegistrateBlockstateProvider prov, BlockState state) {
+        return prov.models().getExistingFile(prov.modLoc("block/torsion_motor"));
+    }
+}
--- src/main/java/com/chaosteam/chaosnkinetics/registry/CKBlockEntityTypes.java
+++ src/main/java/com/chaosteam/chaosnkinetics/registry/CKBlockEntityTypes.java
@@ -1,6 +1,19 @@
 package com.chaosteam.chaosnkinetics.registry;
 
+import com.chaosteam.chaosnkinetics.ChaosKinetics;
+import com.chaosteam.chaosnkinetics.content.kinetics.torsion_motor.TorsionMotorBlockEntity;
+import com.simibubi.create.content.kinetics.base.ShaftRenderer;
+import com.simibubi.create.content.kinetics.base.SingleAxisRotatingVisual;
+import com.tterrag.registrate.util.entry.BlockEntityEntry;
+
 public class CKBlockEntityTypes {
+    public static final BlockEntityEntry<TorsionMotorBlockEntity> TORSION_MOTOR = ChaosKinetics.REGISTRATE
+            .blockEntity("torsion_motor", TorsionMotorBlockEntity::new)
+            .visual(() -> SingleAxisRotatingVisual::shaft, false)
+            .validBlocks(CKBlocks.TORSION_MOTOR)
+            .renderer(() -> ShaftRenderer::new)
+            .register();
+
     public static void register() {
     }
 }
